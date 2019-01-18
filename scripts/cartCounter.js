0a3620bf4c66e70c36d5b1609b4ba0cd066bf43d
Andy146 <anders.engeroen@gmail.com>
2019-01-18 12:02:57 +0100

    Create cartCounter.js

---
+++ scripts/cartCounter.js
@@ -0,0 +1,39 @@
+counter = document.getElementById('cartCounter');
+
+
+if(lStorage.getItem("counted") != undefined) {
+  countedItems = lStorage.getItem("counted");
+  countedItems = parseInt(countedItems, 10);
+  counter.innerHTML = countedItems;
+}
+else {
+  countedItems = counter.innerHTML;
+  countedItems = parseInt(countedItems, 10);
+  counter.innerHTML = countedItems;
+}
+
+
+
+interval = setInterval(changeCounter, 100);
+function changeCounter() {
+  if(counter.innerHTML == 0) {
+    counter.style.display = "none";
+  }
+  else {
+    counter.style.display = "inline-block";
+  }
+}
+
+function cartCounter(x) {
+  if(lStorage.getItem("counted") != undefined) {
+    countedItems = lStorage.getItem("counted");
+  }
+  else {
+    countedItems = counter.innerHTML;
+  }
+
+  countedItems = parseInt(countedItems, 10);
+  countedItems++;
+  counter.innerHTML = countedItems;
+  lStorage.setItem("counted", countedItems);
+}
