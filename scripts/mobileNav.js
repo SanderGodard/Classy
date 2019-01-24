b0719f68352ddeb5a76a98536f08a8fff97d30ac
Sander Godard <sander@godard.no>
2019-01-24 11:15:20 +0100

    Fixed mobilenav

--- scripts/mobileNav.js
+++ scripts/mobileNav.js
@@ -1,19 +1,20 @@
-var dropDownH = 200;
+var dropDownH = "90vh";
 
 function menuToggle() {
   var x = document.getElementById("mobileNav");
-  if (x.style.height === dropDownH + "px") {
+  if (x.style.height === dropDownH) {
     x.style.height = "0px";
-    x.style.display = "none";
 
   } else {
-    x.style.height = dropDownH + "px";
-    x.style.display = "block";
+    x.style.height = dropDownH;
   }
   var a = document.getElementById("mobileMenu");
   if (a.innerHTML === "menu") {
+    a.style.color = "grey";
     a.innerHTML = "clear";
   } else {
+    a.style.color = "black";
     a.innerHTML = "menu";
   }
+  document.body.style.overflow = "hidden";
 }
