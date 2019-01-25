913746431d11843fde2bceeb3715623ffabbddbd
Fredrik Glemmestad <fredrik.glemmestad@hotmail.com>
2019-01-25 10:54:37 +0100

    Description

--- produkter/menn/gensere/placeholder.php
+++ produkter/menn/gensere/placeholder.php
@@ -29,7 +29,23 @@
 
   <div class="hoyreside">
     <h2><a class="Supreme">Cat In The Hat Hettegenser</a></h2>
-  </div>
+
+    <div class="rate">
+        <input type="radio" id="star5" name="rate" value="5" />
+        <label for="star5" title="text">5 stars</label>
+        <input type="radio" id="star4" name="rate" value="4" />
+        <label for="star4" title="text">4 stars</label>
+        <input type="radio" id="star3" name="rate" value="3" />
+        <label for="star3" title="text">3 stars</label>
+        <input type="radio" id="star2" name="rate" value="2" />
+        <label for="star2" title="text">2 stars</label>
+        <input type="radio" id="star1" name="rate" value="1" />
+        <label for="star1" title="text">1 star</label>
+      </div>
+
+    <h2><a> 3000,-</a></h2>
+
+
 
 </div>
 
