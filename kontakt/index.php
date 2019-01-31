c13865d00cba1829bf7783783cec7d762816a9fd
ElineBlakli <elineblakli@hotmail.com>
2019-01-31 09:58:25 +0100

    Jobber på "Om oss" side

--- kontakt/index.php
+++ kontakt/index.php
@@ -13,15 +13,39 @@
      ?>
   </nav>
   <main>
+<div class="Kontakt">
 
+  <div class="KontaktOss">
+    <h1>Kontakt Oss</h1><br>
+    <p>
+      Telefon 941 388 89 <br>
+      Epost hypeit-kundreservice@hotmail.com <br>
+      Instagram @hypeit_ <br>
+      Facebook "REPLACE ME"
 
+    </p>
+  </div>
+  <div class="OmOss">
+    <h1> Om Oss </h1><br>
+    <p>
+      HypeIT er den første norske nettbutikken som kan <br> tilby et stort utvalg
+      hypebeast klær. <br> Vi har signert kontrakter med flere <br> store hypebeast-merker
+      som <br> gjør at vi kan tilby bedre priser <br> enn konkurenten. I tillegg vil alle
+      varer <br> være lagret i Norge som gjør <br> at du som kunde ikke trenger å betale <br>
+      frakt eller toll. Som om ikke det var nok fører <br> denne avtalen til at vi
+      vil ha tilgang til alle <br> nye produkter, og vil ofte oppdatere vårt <br> utvalg
+      av hypebeast klær.
+    </p>
+  </div>
+
+</div>
 
 
 
   </main>
   <footer>
     <?php
-      include "../elements/footer.php";
+      //include "../elements/footer.php";
      ?>
   </footer>
 </body>
