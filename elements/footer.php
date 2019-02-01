180a0fd216c385553fd6d3c9f7f3dff433b90d4b
Eduard Prokhorikhin <eduardprokhorikhin@gmail.com>
2019-02-01 12:32:12 +0100

    fooooooooterr

--- elements/footer.php
+++ elements/footer.php
@@ -5,39 +5,53 @@
   <div class="footer">
   <div class="topFooter">
     <div class="tf_tekst">
-      Kom i kontakt med oss
+      <a href="#">Kom i kontakt med oss</a>
     </div>
     <div class="icon_wrapper">
+      <a href="#">
     <div class="footer_icon">
 <img src="/HypeIT/images/facebookicon.png" alt="icon">
 </div>
+</a>
+<a href="#">
 <div class="footer_icon">
 <img src="/HypeIT/images/instagramicon.png" alt="icon">
     </div>
+  </a>
   </div>
 </div>
   <div class="centerFooter">
     <div class="cf_wrapper">
       <div class="cf_kolonne">
-        <h1>HypeIT</h1><br><br><br>
+        <h1><a href="">HypeIT</a></h1>
+        <div class="kolonne_txt">
         Lorem ipsum dolor sit amet, consectetur
         adipiscing elit, sed do eiusmod tempor
         incididunt ut labore et dolore magna aliqua.
          Ut enim ad minim veniam, quis nostrud
          exercitation ullamco laboris nisi ut aliquip
          ex ea commodo consequat. Duis aute irure dolor
-
       </div>
+    </div>
       <div class="cf_kolonne">
 
       </div>
       <div class="cf_kolonne">
-        <h1>Kontakt oss</h1>
+        <h1><a href="">Kontakt oss</a></h1>
+        <div class="kolonne_txt">
+          <ul>
+            <li><a href=""><img src="/HypeIT/images/homeicon.png" alt="home"></a><a href=""><div> Snertingdal, Oppland, Norge </div></a></li>
+            <li><a href=""><img src="/HypeIT/images/mailicon.png" alt="home"></a><a href=""><div> Hypeit-kundeservice@hotmail.com</div></a></li>
+            <li><a href=""><img src="/HypeIT/images/phoneicon.png" alt="home"></a><a href=""><div> +47 474 54 829</div></a></li>
+          </ul>
+        </div>
       </div>
     </div>
     </div>
   <div class="bottomFooter">
-    &copy Copyright HypeIT AS
+    <div>
+      &copy Copyright HypeIT AS
+    </div>
   </div>
   </div>
 </footer>
