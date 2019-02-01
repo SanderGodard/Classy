31a445d3a23db338119ef346163857b9e48c4799
chinosonic <Qsw57AB1DvaK>
2019-02-01 10:21:14 +0100

    b

--- store/cart/index.php
+++ store/cart/index.php
@@ -15,9 +15,35 @@
   <main>
 
 
-<!--
-  Write code here
--->
+<div class="cartProducts">
+<img src="../../images/SupremeCatInAHatBrightRoyal.jpg" alt="Produkt bilde">
+<div class="cartText">
+<p id="cartProductName">Cat in the Hat Hettegenser</p>
+<p id="cartProductPrice">3000 kr</p>
+<p id="cartProductColor">Farge: Royal Blue<p>
+<p id="cartProductSize">Størrelse: L</p>
+<input type="text" name="navn" id="cartProductNumber" value="1">
+</div>
+</div>
+
+<div class="cartProducts">
+<img src="../../images/SupremeCatInAHatMustard.jpg" alt="Produkt bilde">
+<div class="cartText">
+<p id="cartProductName">Supreme Cat In a Hat </p>
+<p id="cartProductPrice">3000 kr</p>
+<p id="cartProductColor">Farge: Royal Blue</p>
+<p id="cartProductSize">Størrelse: L</p>
+<input type="text" name="navn" id="cartProductNumber" value="1">
+</div>
+</div>
+
+
+<div class="cartConfirmation">
+<div class="cartConfDiscount">
+<p class="cartConfDiscountText">Legg til en rabattkode</p>
+<input type="text" name="navn" id="cartConfDiscountInput">
+</div>
+</div>
 
 
   </main>
