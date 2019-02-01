148aad3a97bb7778518d8ee1c3bb8c05e2b5cd3e
Sander Godard <sander@godard.no>
2019-02-01 21:58:17 +0100

    Cleaned up some fredrik spaghet.
    Nå må du bare adde media quiries tror jeg. Har ikke testet så mye

--- produkter/menn/gensere/index.php
+++ produkter/menn/gensere/index.php
@@ -16,27 +16,26 @@
 
     <div class="produktside">
 
-  <div class="venstreside">
-    <div class="Bilder">
-      <img src="../../../images/racks.jpg" class="StortBilde" id="storBilde">
-      <div class="smaBilder" id="Gliscor">
-        <img src="../../../images/racks.jpg" alt="Rack" id="img1">
-        <img src="../../../images/gul.jpg" alt="Green" id="img2">
-        <img src="../../../images/grønn.jpg" alt="Mustard" id="img3">
+      <div class="venstreside">
+        <div class="Bilder">
+          <img src="../../../images/racks.jpg" class="StortBilde" id="storBilde">
+          <div class="smaBilder" id="Gliscor">
+            <img src="../../../images/racks.jpg" alt="Rack" id="img1">
+            <img src="../../../images/gul.jpg" alt="Green" id="img2">
+            <img src="../../../images/grønn.jpg" alt="Mustard" id="img3">
+          </div>
+        </div>
       </div>
 
       <div class="hoyreside">
         <h2><a class="Supreme">Cat In The Hat Hettegenser</a></h2>
-
         <h2><a class="pris"> 3000,-</a></h2>
-
         <div class="str">
           <a>S</a>
           <a>M</a>
           <a>L</a>
           <a>XL</a>
         </div>
-
         <div class="farge">
           <a id="farge1"></a>
           <a id="farge2"></a>
