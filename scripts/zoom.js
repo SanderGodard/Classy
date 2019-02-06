2b9d1d4e9d91a132a55525a3a7f7331a5d5263cf
Andy146 <anders.engeroen@gmail.com>
2019-02-06 12:12:53 +0100

    Update zoom.js

--- scripts/zoom.js
+++ scripts/zoom.js
@@ -1,36 +1,38 @@
 
 if(document.getElementById('storBilde') != null) {
 
-  var zoomImg = document.getElementById('storBilde');
+  zoomImg = document.getElementById('storBilde');
 
-  var bigImg = document.createElement('img');
-  bigImg.src = img.src;
-  bigImg.id = "bigImg";
+  bigImg = document.createElement('img');
+  bigImg.src = zoomImg.src;
 
   var style = bigImg.style;
 
-  style.position = "fixed";
-  style.height = "100%";
+  style.position = "absolute";
+  style.zIndex = "900000000";
+  style.height = "80%";
   //style.width = "30%";
-  style.top = "0";
-  style.bottom = "0";
-  style.left = "20%";
-  style.right = "20%";
+  style.top = "10%";
+  style.bottom = "10%";
+  style.left = "25%";
+  style.right = "25%";
+  style.border = "1px solid black";
+  style.borderRadius = "5px";
 
   zoomImg.addEventListener("click", zoom);
-
 }
 
 
 function zoom() {
+  bigImg.src = zoomImg.src;
   document.body.appendChild(bigImg);
   document.addEventListener("click", unZoom, event);
   console.log("hello");
 }
 
 function unZoom(x) {
-  if(x.target != document.getElementById('bigImg')) {
-    bigImg.parentNode.removeChild(document.getElementById('bigImg'));
+  if(x.target != document.getElementById('storBilde')) {
+    document.body.removeChild(bigImg);
     document.removeEventListener("click", unZoom);
   }
 
