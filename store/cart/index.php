954dbe4e52244f31ec7d23091516da8da657d39f
vidar <vidarenge@gmail.com>
2019-02-08 12:23:24 +0100

    shopping cart greier
    ja

--- store/cart/index.php
+++ store/cart/index.php
@@ -20,8 +20,10 @@
     <div class="cartProducts">
       <img src="../../images/SupremeCatInAHatBrightRoyal.jpg" alt="Produkt bilde">
       <div class="cartText">
-        <p id="cartProductName">Cat in the Hat Hettegenser</p>
-        <p id="cartProductPrice">3000 kr</p>
+        <div class="cartTextChild">
+          <p id="cartProductName">Cat in the Hat Hettegenser</p>
+          <p id="cartProductPrice">3000 kr</p>
+        </div>
         <div class="cartProductAndPriceParent">
           <div class="cartProductAndPrice">
             <p id="cartProductColor">Farge:</p>
@@ -41,8 +43,10 @@
     <div class="cartProducts">
       <img src="../../images/SupremeCatInAHatBrightRoyal.jpg" alt="Produkt bilde">
       <div class="cartText">
-        <p id="cartProductName">Cat in the Hat Hettegenser</p>
-        <p id="cartProductPrice">3000 kr</p>
+        <div class="cartTextChild">
+          <p id="cartProductName">Cat in the Hat Hettegenser</p>
+          <p id="cartProductPrice">3000 kr</p>
+        </div>
         <div class="cartProductAndPriceParent">
           <div class="cartProductAndPrice">
             <p id="cartProductColor">Farge:</p>
@@ -59,7 +63,28 @@
       </div>
     </div>
 
-
+    <div class="cartProducts">
+      <img src="../../images/SupremeCatInAHatBrightRoyal.jpg" alt="Produkt bilde">
+      <div class="cartText">
+        <div class="cartTextChild">
+          <p id="cartProductName">Cat in the Hat Hettegenser</p>
+          <p id="cartProductPrice">3000 kr</p>
+        </div>
+        <div class="cartProductAndPriceParent">
+          <div class="cartProductAndPrice">
+            <p id="cartProductColor">Farge:</p>
+            <p id="cartProductSize">Størrelse:</p>
+          </div>
+          <div class="cartProductAndPrice2">
+            <p id="cartProductColor2">Blå</p>
+            <p id="cartProductSize2">L</p>
+          </div>
+        </div>
+        <div class="cartProductNumberDiv">
+          <input type="text" name="navn" id="cartProductNumber" value="1">
+        </div>
+      </div>
+    </div>
   </div>
 
   <div class="cartConfirmation">
@@ -70,27 +95,25 @@
         <button type="button">Bruk</button>
       </div>
       <a href="#"><p class="cartConfDiscountSmallText">*Les mer om friendkode her!</p></a>
-      <div class="cartPaymentCenter">
-        <div class="cartPayment">
-          <div class="cartPaymentChild">
-            <p>Bestillingsverdi:</p>
-            <p>Levering:</p>
-            <p>Rabatt:</p>
-            <br>
-            <p>Sum</p>
-          </div>
-          <div class="cartPaymentChild2">
-            <p>1000kr</p>
-            <p>1000kr</p>
-            <p>1000kr</p>
-            <br>
-            <p>20000,95kr</p>
-          </div>
-        </div>
-        <div class="cartButtonCenter">
-          <button id="videreTilKassenButton" type="button">Gå videre til kassen</button>
-        </div>
+    </div>
+    <div class="cartPayment">
+      <div class="cartPaymentChild">
+        <p>Bestillingsverdi:</p>
+        <p>Levering:</p>
+        <p>Rabatt:</p>
+        <br>
+        <p>Sum</p>
       </div>
+      <div class="cartPaymentChild2">
+        <p>1000kr</p>
+        <p>1000kr</p>
+        <p>1000kr</p>
+        <br>
+        <p>20000,95kr</p>
+      </div>
+    </div>
+    <div class="cartButtonCenter">
+      <button id="videreTilKassenButton" type="button">Gå videre til kassen</button>
     </div>
   </div>
 </div>
